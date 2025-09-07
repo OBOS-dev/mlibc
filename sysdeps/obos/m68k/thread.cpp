db32c8b6065433b88f95d385431099a912d9cdec
Omar Berrow <omarkberrow@gmail.com>
2025-09-07 17:07:06 -0400

    sysdeps/obos: Fix a bug with __m68k_read_tp()

--- sysdeps/obos/m68k/thread.cpp
+++ sysdeps/obos/m68k/thread.cpp
@@ -1,11 +1,13 @@
 #include <obos/syscall.h>
+#include <mlibc/tcb.hpp>
 
 #define Sys_SetTCB 0x80000000
 #define Sys_GetTCB 0x80000001
 
 extern "C" void* __m68k_read_tp()
 {
-    return (void*)syscall0(Sys_GetTCB);
+    // see mlibc::get_current_tcb
+    return (void*)(syscall0(Sys_GetTCB) + 0x7000 + sizeof(Tcb));
 }
 
 namespace mlibc {
