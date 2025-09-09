978c7f614fac41a1c50546efe45539e5347d20ae
Omar Berrow <omarkberrow@gmail.com>
2025-09-09 17:59:14 -0400

    sysdeps/obos: Mark __m68k_read_tp as hidden in rtld

--- sysdeps/obos/m68k/thread.cpp
+++ sysdeps/obos/m68k/thread.cpp
@@ -4,7 +4,11 @@
 #define Sys_SetTCB 0x80000000
 #define Sys_GetTCB 0x80000001
 
-extern "C" void* __m68k_read_tp()
+extern "C" 
+#if MLIBC_BUILDING_RTLD
+__attribute__((visibility("hidden")))
+#endif
+void* __m68k_read_tp()
 {
     // see mlibc::get_current_tcb
     return (void*)(syscall0(Sys_GetTCB) + 0x7000 + sizeof(Tcb));
